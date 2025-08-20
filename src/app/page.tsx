2f6c715ec4b6cd02f8333064d2c9b0a5b558915e
rappo-tech <rappo7292@gmail.com>
2025-08-20 21:40:31 +0530

    added tech stack button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,8 +10,13 @@ export  default function  Home() {
 return (<div>
 
 <Navbar></Navbar>
+
 <Link href={'/create'}>
 <Button></Button>
+</Link>
+
+<Link href={'https://medium.com/@anuragdnd456/13august2025-fullstack-9e89202de1cd'}>
+<button  className="bg-amber-500 hover:bg-amber-400 px-4 py-4      my-6 block mx-auto ">tech stack</button>
 </Link>
 
 </div>)
