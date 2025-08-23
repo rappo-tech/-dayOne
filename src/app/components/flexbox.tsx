d601210beccba6ec2b77356903fa6c3cb3fb34d6
rappo-tech <rappo7292@gmail.com>
2025-08-23 14:03:08 +0530

    added intial  flex-box

---
+++ src/app/components/flexbox.tsx
@@ -0,0 +1,15 @@
+'use client'
+export default function Flexbox() {
+return (<div>
+
+<div className="flex flex-row  gap-x-2 justify-between items-start border-amber-800 bg-amber-300 pl-2 pr-2  pt-4 pb-10 ">
+<div className="bg-green-500 h-84 w-1/4 md:w-1/2 sm:-1/3 lg:w-full my-0 ">websocket</div>
+<div className="bg-violet-400 h-84 w-1/4 md:w-1/2 sm:-1/3 lg:w-full my-0 ">webRTC</div>
+<div className="bg-red-400  h-84 w-1/4 md:w-1/2 sm:-1/3 lg:w-full my-0 ">code... </div>
+</div>
+
+
+
+
+</div>)
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,13 @@
 import Link from "next/link"
 import Navbar from "./components/navbar"
 import Button from "./components/button"
+import Flexbox from "./components/flexbox"
 
 export  default function  Home() {
 
 
 return (<div>
+
 
 <Navbar></Navbar>
 
@@ -18,6 +20,9 @@ return (<div>
 <Link href={'https://medium.com/@anuragdnd456/13august2025-fullstack-9e89202de1cd'}>
 <button  className="bg-amber-500 hover:bg-amber-400 px-4 py-4      my-6 block mx-auto ">tech stack</button>
 </Link>
+
+<Flexbox></Flexbox>
+
 
 </div>)
   
