a26bfd43189227864ddf9e2169525d175304b8ab
rappo-tech <rappo7292@gmail.com>
2025-09-04 19:07:27 +0530

    added vibration

--- src/app/components/button.tsx
+++ src/app/components/button.tsx
@@ -2,7 +2,12 @@ export  default function  Button() {
     
 
 return (<div>
-    <button className="bg-teal-600 hover:bg-teal-400 w-full     block sm:w-1/2 md:w-1/3 lg:w-1/4 mx-auto my-6 p-3 " >capitalize it !!! </button>
+    <button  onClick={()=>{
+  if ("vibrate" in navigator) {
+    navigator.vibrate(200);
+  } else {
+    alert("Vibration not supported on this device");
+     }} }   className="bg-teal-600 hover:bg-teal-400 w-full     block sm:w-1/2 md:w-1/3 lg:w-1/4 mx-auto my-6 p-3 " >capitalize it !!! </button>
 </div>)
 
 }
